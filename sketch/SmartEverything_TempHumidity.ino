1a0ce814cca294d86a31e7ce2306dc662fa65765
Alex Bucknall <alex.bucknall@gmail.com>
2017-01-18 22:12:54 +0100

    fixed some changes

--- sketch/SmartEverything_TempHumidity.ino
+++ sketch/SmartEverything_TempHumidity.ino
@@ -1,7 +1,7 @@
 /*
     @aureleq
     Read Temp/Humidity value and send values over Sigfox everytime the board is powered-on or reset button is pressed
-    
+
     Telit le51-868-s more information available here:
     http://www.telit.com/products/product-service-selector/product-service-selector/show/product/le51-868-s/
 */
@@ -13,30 +13,30 @@
 
 #define DEBUG 0
 
-// the setup function runs once when you press reset or power the board
+// The setup function runs once when you press reset or power the board
 void setup() {
     if(DEBUG) {
       while(!SerialUSB);
     }
     SerialUSB.begin(9600);
 
-    //Initiate the Wire library and join the I2C bus
+    // Initiate the Wire library and join the I2C bus
     Wire.begin();
     // Init humidity sensor
     smeHumidity.begin();
-    
+
     /* ------- SIGFOX INIT - BEGIN ------- */
     sfxAntenna.begin();
     int initFinish=1;
 
     SerialUSB.println("SFX in Command mode");
-    sfxAntenna.setSfxConfigurationMode(); // enter in configuration Mode
+    sfxAntenna.setSfxConfigurationMode(); // Enter into configuration Mode
 
     do {
         uint8_t answerReady = sfxAntenna.hasSfxAnswer();
         if (answerReady){
             switch (initFinish){
-            case 1:                                
+            case 1:
                 SerialUSB.println("SFX in Data mode");
                 sfxAntenna.setSfxDataMode();
                 initFinish++;
@@ -49,34 +49,33 @@ void setup() {
         }
     } while (initFinish!=3);
     /* ------- SIGFOX INIT - END ------- */
+}
 
+// The loop function runs over and over again forever
+void loop() {
 
     // Read Temp/Humidity
-    double hum = 0;
+    float hum;
     hum = smeHumidity.readHumidity();
     SerialUSB.print("Humidity   : ");
     SerialUSB.print(hum);
     SerialUSB.println(" %");
 
-    double temp = 0;
+    float temp;
     temp = smeHumidity.readTemperature();
     SerialUSB.print("Temperature: ");
     SerialUSB.print(temp);
     SerialUSB.println(" celsius");
 
-    // concat values to payload
+    // Concat values to payload
     char payload[4];
-    sprintf(payload, "%d%d", (int)hum, (int)temp);
-    
+    sprintf(payload, "%d%d", (int8_t)hum, (int8_t)temp);
+
     // Send Temp/Humidity over Sigfox
     // sfxSendData() is sub-optimal as it transforms char string to hex
     SerialUSB.println("sending data over the network");
     ledGreenLight(HIGH);
     sfxAntenna.sfxSendData(payload, strlen((char*)payload));
-}
-
-// the loop function runs over and over again forever
-void loop() {
 
     bool answerReady = sfxAntenna.hasSfxAnswer();
 
